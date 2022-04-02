9c6a5b11584866a6cf31f5f4757be7895f26aadd
SupremeDonut <dywang200705@gmail.com>
2022-04-02 15:04:04 -0500

    made basic nav thing

--- src/App.js
+++ src/App.js
@@ -9,9 +9,33 @@ function App() {
 		<header>
 			<h1>Climate Change</h1>
 			<nav>
-				<div className="nav-item">Land</div>
-				<div className="nav-item">Air</div>
-				<div className="nav-item">Sea</div>
+				<div className="nav-item">
+					<h2>Land</h2>
+					<ul className="nav-hover">
+						<li><a href="#">Land Overuse</a></li>
+						<li><a href="#">Land Pollution</a></li>
+						<li><a href="#">Deforestation</a></li>
+					</ul>
+				</div>
+				<div className="nav-item">
+					<h2>Air</h2>
+					<ul className="nav-hover">
+						<li><a href="#">Air Pollution</a></li>
+						<li><a href="#">Ozone</a></li>
+						<li><a href="#">Temperature</a></li>
+					</ul>
+				</div>
+				<div className="nav-item">
+					<h2>Sea</h2>
+					<ul className="nav-hover">
+						<li><a href="#">Sea Levels</a></li>
+						<li><a href="#">Marine Life</a></li>
+						<li><a href="#">Plastic in the Ocean</a></li>
+					</ul>
+				</div>
+				<div className="nav-item">
+					<h2>Learn More</h2>
+				</div>
 			</nav>
 		</header>
 		<Land />
