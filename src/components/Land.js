1bf28ef61cf0778b574a89d69ab6e78896c53ae9
Rishi Vemuri <rishikvemuri@gmail.com>
2022-04-02 22:59:35 -0500

    //Put in text

--- src/components/Land.js
+++ src/components/Land.js
@@ -22,17 +22,12 @@ function Land() {
 </p>
 		</div>
 		<div className="subsection land-pollution">
-		<h1>Why global warming is deadly throughout people
-</h1>
-			<p> Heat waves caused by global warming present greater risk of heat-related illness and death, most frequently among people who have diabetes who are elderly or are very young.
-
-</p>
+		<h1>Why global warming is deadly throughout people</h1>
+			<p>Heat waves caused by global warming present greater risk of heat-related illness and death, most frequently among people who have diabetes who are elderly or are very young.</p>
 		</div>
 		<div className="subsection deforestation">
-		<h1> Rainforest destruction is a major cause of carbon dioxide release
-</h1>
-			<p>When they are degraded or completely cleared, e.g. by fire - a process referred to as deforestation - this stored carbon has the potential to be released back into the atmosphere as carbon dioxide and contribute to climate change.
-</p>
+		<h1> Rainforest destruction is a major cause of carbon dioxide release</h1>
+			<p>When they are degraded or completely cleared, e.g. by fire - a process referred to as deforestation - this stored carbon has the potential to be released back into the atmosphere as carbon dioxide and contribute to climate change.</p>
 		</div>
 		interactive thing that will feature people doing stuff
 	</div>
