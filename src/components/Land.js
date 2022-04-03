9099d0e6af4edbabcc904b50e085032d97f9eda4
Rishi Vemuri <rishikvemuri@gmail.com>
2022-04-02 22:42:35 -0500

    Merge branch 'master' of https://github.com/SupremeDonut/shshacks-2022

--- src/components/Land.js
+++ src/components/Land.js
@@ -17,16 +17,22 @@ function Land() {
 			</div>
 		</div>
 		<div className="subsection overuse">
-		<h1>Title</h1>
-			<p>Info</p>
+		<h1>Climate change affects soil</h1>
+			<p> Soil moisture has significantly decreased in the Mediterranean region and increased in parts of northern Europe since the 1950s. Continuing declines in soil moisture can increase the need for irrigation in agriculture and lead to smaller yields and even desertification, with potentially dramatic impacts on food production.
+</p>
 		</div>
 		<div className="subsection land-pollution">
-		<h1>Title</h1>
-			<p>Info</p>
+		<h1>Why global warming is deadly throughout people
+</h1>
+			<p> Heat waves caused by global warming present greater risk of heat-related illness and death, most frequently among people who have diabetes who are elderly or are very young.
+
+</p>
 		</div>
 		<div className="subsection deforestation">
-		<h1>Title</h1>
-			<p>Info</p>
+		<h1> Rainforest destruction is a major cause of carbon dioxide release
+</h1>
+			<p>When they are degraded or completely cleared, e.g. by fire - a process referred to as deforestation - this stored carbon has the potential to be released back into the atmosphere as carbon dioxide and contribute to climate change.
+</p>
 		</div>
 		interactive thing that will feature people doing stuff
 	</div>
--- src/components/Smoke.js
+++ src/components/Smoke.js
@@ -1,5 +1,4 @@
 import React from "react"
-
 class Smoke extends React.Component {
 	constructor(props) {
 		super(props);
