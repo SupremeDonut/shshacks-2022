712476ee00e90ae2e5852ef83c4e3f0ca7086a18
JRC-DXRK <burneracc489@gmail.com>
2022-04-03 11:09:14 -0500

    image land

--- src/components/Land.js
+++ src/components/Land.js
@@ -1,34 +1,32 @@
 import '../styles/Card.css';
 import '../styles/Land.css';
+import car from '../assets/car.png'
 
 function Land() {
 	return (
-	<div className="land page">
+	<div className="land page" id="land">
 		<h2>Land</h2>
 		<div className="land-info info">
-			<div className="land-card card">
+			<div className="land-card1 card">
 				<h2>Shocking Fact</h2>
 			</div>
-			<div className="land-card card">
+			<div className="land-card2 card">
 				<h2>Shocking Fact</h2>
 			</div>
-			<div className="land-card card">
+			<div className="land-card3 card">
 				<h2>Shocking Fact</h2>
 			</div>
 		</div>
 		<div className='padding'>
-		<div id = 'soil'></div>
-		<div className="subsection overuse">
+		<div className="subsection overuse" id="overuse">
 			<h1>Climate change affects soil</h1>
 			<p> Soil moisture has significantly decreased in the Mediterranean region and increased in parts of northern Europe since the 1950s. Continuing declines in soil moisture can increase the need for irrigation in agriculture and lead to smaller yields and even desertification, with potentially dramatic impacts on food production.</p>
 		</div>
-		<div id = 'global_warming'></div>
-		<div className="subsection land-pollution">
+		<div className="subsection land-pollution" id="lpoll">
 			<h1>Why global warming is deadly throughout people</h1>
 			<p>Heat waves caused by global warming present greater risk of heat-related illness and death, most frequently among people who have diabetes who are elderly or are very young.</p>
 		</div>
-		<div id = 'deforest'></div>
-		<div className="subsection deforestation">
+		<div className="subsection deforestation" id="deforest">
 			<h1> Rainforest destruction is a major cause of carbon dioxide release</h1>
 			<p>When they are degraded or completely cleared, e.g. by fire - a process referred to as deforestation - this stored carbon has the potential to be released back into the atmosphere as carbon dioxide and contribute to climate change.</p>
 		</div>
